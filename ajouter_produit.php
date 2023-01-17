c6dac2ac83975bb7f10760b6e8823bd8aa661985
epeeepeeViolette <dylanoiknine@outlook.fr>
2023-01-17 14:32:35 +0100

    image

--- ajouter_produit.php
+++ ajouter_produit.php
@@ -5,12 +5,13 @@ if (isset($_POST['ajouter'])) {
 
     $lien = connect_to_db();
 
-    $stmt = $lien->prepare('INSERT INTO miel (nom_miel,type_miel,provenance_miel,description_miel,image) values (:nom_miel,:type_miel,:provenance_miel,:description_miel,:image)');
+    $stmt = $lien->prepare('INSERT INTO miel (nom_miel,type_miel,provenance_miel,description_miel,image,prix) values (:nom_miel,:type_miel,:provenance_miel,:description_miel,:image,:prix)');
     $stmt->bindValue(':nom_miel', $_POST['nom_miel'], PDO::PARAM_STR);
     $stmt->bindValue(':type_miel', $_POST['type_miel'], PDO::PARAM_STR);
     $stmt->bindValue(':provenance_miel', $_POST['provenance_miel'], PDO::PARAM_STR);
     $stmt->bindValue(':description_miel', $_POST['description_miel'], PDO::PARAM_STR);
     $stmt->bindValue(':image', $_POST['image'], PDO::PARAM_STR);
+    $stmt->bindValue(':prix', $_POST['prix'], PDO::PARAM_STR);
     $stmt->execute();
 
     $erreur="";
@@ -59,8 +60,12 @@ if (isset($_POST['ajouter'])) {
             <!--------------à voir en cours pour recuperer une image à telecharger ----------------->
             <div class="row">
                 <div class="col-12">
-                    <label>Image:</label>
-                    <input type="image" name="image"/>
+                    <label for="image" class="form-label">Image du produit:</label>
+                    <input class="form-control" type="file" id="image" name="image">
+                </div>
+                <div class="col-12">
+                    <label>Prix:</label>
+                    <input type="text" name="prix"/>
                 </div>
             </div>
             <!-------------------------------------------------------------------------------------->
