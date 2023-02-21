6bd7f1af88eb493bbaa88a2d366f84e30ba8cc82
epeeepeeViolette <dylanoiknine@outlook.fr>
2023-02-21 13:52:24 +0100

    dd

--- appli/essai.php
+++ appli/essai.php
@@ -34,27 +34,38 @@
 
         if($count!=0) // nom d'utilisateur et mot de passe correctes
         {
-        echo "SUCCESS%".json_encode($rows);
+            $requete =$lien->prepare("SELECT * FROM eleves where identifiant_eleve= :nom_utilisateur and mdp_eleve= :mdp_utilisateur");  
+            $requete->bindValue(':nom_utilisateur', $login, PDO::PARAM_STR);
+            $requete->bindValue(':mdp_utilisateur', $mdp, PDO::PARAM_STR);
+            $requete->execute();
+            $rows = $requete->fetchAll();
+
+        foreach($rows as $enregistresement){
+            $identifiant_eleve = $enregistresement['identifiant_eleve'];
+            $mdp_eleve = $enregistresement['mdp_eleve'];
+        }
+
+        echo "SUCCESS <br> ".json_encode($identifiant_eleve);
         }
         else
         {
-        echo "ECHEC% Identifiants incorrects";  // utilisateur ou mot de passe incorrect
+        echo "ECHEC Identifiants incorrects";  // utilisateur ou mot de passe incorrect
         }
     }
     else
     {
-    echo "ECHEC% Tous les champs sont requis" ; // utilisateur ou mot de passe vide
+    echo "ECHEC Tous les champs sont requis" ; // utilisateur ou mot de passe vide
     }
 }
 else
 {
-    echo "ECHEC% Echec de connexion à la base de données";
+    echo "ECHEC Echec de connexion à la base de données";
 }
 
 
 
 
-    echo 'bonjour';
+    echo '<br> Bonjour';
 
 
 
