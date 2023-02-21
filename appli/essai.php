5c46d7aa2971cb05a4bf506a2a2240c956dddd17
epeeepeeViolette <dylanoiknine@outlook.fr>
2023-02-21 14:39:41 +0100

    ll

--- appli/essai.php
+++ appli/essai.php
@@ -3,8 +3,8 @@
 
     
 
-    $_POST['login'] = 'dylan98';
-    $_POST['password'] = '123456';
+    //$_POST['login'] = 'dylan98';
+    //$_POST['password'] = '123456';
 
 
     if(isset($_POST['login']) && isset($_POST['password'])){
@@ -39,27 +39,31 @@
             $requete->bindValue(':mdp_utilisateur', $mdp, PDO::PARAM_STR);
             $requete->execute();
             $rows = $requete->fetchAll();
-
+            $a =array(); 
         foreach($rows as $enregistresement){
-            $identifiant_eleve = $enregistresement['identifiant_eleve'];
-            $mdp_eleve = $enregistresement['mdp_eleve'];
+            $a['id_eleve'] = $enregistresement['id_eleve'];
+            $a['nom_eleve'] =  $enregistresement['nom_eleve'];
+            $a['prenom_eleve'] =  $enregistresement['prenom_eleve'];
+            $a['classe_eleve'] = $enregistresement['classe_eleve'];
+            $a['identifiant_eleve'] = $enregistresement['identifiant_eleve'];
+            $a['mdp_eleve'] = $enregistresement['mdp_eleve'];
         }
 
-        echo "SUCCESS <br> ".json_encode($identifiant_eleve);
+        echo "SUCCESS% <br> ".json_encode($a);
         }
         else
         {
-        echo "ECHEC Identifiants incorrects";  // utilisateur ou mot de passe incorrect
+        echo "ECHEC% Identifiants incorrects";  // utilisateur ou mot de passe incorrect
         }
     }
     else
     {
-    echo "ECHEC Tous les champs sont requis" ; // utilisateur ou mot de passe vide
+    echo "ECHEC% Tous les champs sont requis" ; // utilisateur ou mot de passe vide
     }
 }
 else
 {
-    echo "ECHEC Echec de connexion à la base de données";
+    echo "ECHEC% Echec de connexion à la base de données";
 }
 
 
